f5641a0c1c85d7d4a09435b04d58fd29f2cec097
Ivana <ivanabbrito@hotmail.com>
2022-10-23 09:25:38 -0300

    exercicio de arrow e map

--- JS/basico/arrow_function/teste.js
+++ JS/basico/arrow_function/teste.js
@@ -1,4 +1,7 @@
  //funcao arrow
+//formato () => {}
+//normalmente utilizada em conjunto com map e pode ser utilizada assim:
+//const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
 const soma = (...num) => {
     let r=0
     for(n of num){
